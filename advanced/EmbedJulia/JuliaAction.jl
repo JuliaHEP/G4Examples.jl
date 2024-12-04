b5fef1fb861d168baaaf8fde76acdac1be1f8e75
Pere Mato <pere.mato@cern.ch>
2024-12-04 13:04:27 +0100

    Avoid name clashes

--- advanced/EmbedJulia/JuliaAction.jl
+++ advanced/EmbedJulia/JuliaAction.jl
@@ -13,10 +13,10 @@ ldflags = ["-Wl,-rpath,$g4prefix/lib", "-Wl,-rpath,$jlprefix/lib"];
 Sys.KERNEL == :Linux  && append!(ldflags, ["-Wl,--no-as-needed"]);
 
 Base.run(`c++ -O2 -fPIC $cflags -I$jlprefix/include/julia $ldflags $g4libs $jllibs
-          -o G4example $(@__DIR__)/G4example.cpp`).exitcode == 0 || error("Compilation failed");
+          -o G4example.exe $(@__DIR__)/G4example.cpp`).exitcode == 0 || error("Compilation failed");
 
 withenv("JULIA_PROJECT" => "@.", "G4NUMTHREADS" => "8") do
-   Base.run(`./G4example`).exitcode == 0 || error("Execution failed")
+   Base.run(`./G4example.exe`).exitcode == 0 || error("Execution failed")
 end
 
 println("=====> The file edepHist.png should have been saved")
